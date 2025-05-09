df241039bc3249b577c04d0c447c2bde89f02620
Mohamed lotfi <mohamed80f@gmail.com>
2025-05-09 18:59:28 +0300

    first commit

---
+++ src/app/course-details/course-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-details',
+  imports: [],
+  templateUrl: './course-details.component.html',
+  styles: ``
+})
+export class CourseDetailsComponent {
+
+}
